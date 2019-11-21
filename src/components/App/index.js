2f511bf4c710e9f51feb52fe12e85ec1c03e1412
Nazmi <nazmifeeroz@hotmail.com>
2019-11-21 16:46:25 +0800

    :wrench: uses materializecss for styles

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,26 +1,5 @@
 import React from 'react'
-import {Card} from 'react-bootstrap'
 
-const App = () => (
-  <div className="container">
-    <div className="row mt-5">
-      <div className="col-md-6 mx-auto">
-        <h1 className="text-center mb-3">Gallereasy</h1>
-        <Card>
-          <Card.Header>Some header.</Card.Header>
-          <Card.Body>
-            <Card.Subtitle className="mb-4 text-muted">
-              Some subtitle.
-            </Card.Subtitle>
-            <Card.Text>Some Body text</Card.Text>
-            <Card.Link className="text-success" href="!#">
-              Some Link
-            </Card.Link>
-          </Card.Body>
-        </Card>
-      </div>
-    </div>
-  </div>
-)
+const App = () => <div className="container">Hello Gallereasy</div>
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import 'bootstrap/dist/css/bootstrap.css'
+import 'materialize-css/dist/css/materialize.min.css'
+
 import App from '@components/App'
 
 const mountApp = document.getElementById('app')
